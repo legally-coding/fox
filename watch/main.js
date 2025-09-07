7a13f7a04ae016ed80c1d47e76127fa77b36cf87
legally-coding <166270607+legally-coding@users.noreply.github.com>
2025-09-07 13:41:06 -0700

    auto quality
    trying to fix the auto qualtiy and for some reason the m3u8 not fetching anymore - when in doubt, set query param quality=auto for it not to break - also better for network instability

--- watch/main.js
+++ watch/main.js
@@ -7,8 +7,9 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 let awesomestreamid = params?.stream;
 let cors = params?.cors;
 let quality = params?.quality || "5";
+let autoquality = params?.quality == "auto";
 
-if(awesomestreamid)startVideo();
+if (awesomestreamid) startVideo();
 
 
 function getQuality(m3, currentGame) {
@@ -16,7 +17,7 @@ function getQuality(m3, currentGame) {
     console.log(m3);
     let m3u8;
     for (let i = 0; i < m3.length; i++) {
-        if (m3[i].includes(`${quality}.m3u8`) || m3[i].includes(`h.m3u8`))m3u8 = m3[i];
+        if (m3[i].includes(`${quality}.m3u8`) || m3[i].includes(`h.m3u8`)) m3u8 = m3[i];
     }
     console.log(m3u8);
     //Sets the quality of the video
@@ -32,8 +33,8 @@ async function initLogin() {
             test = test.split("");
             test[i] = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
             test = test.join("");
-        }else if(!isNaN(did[i])){
-            test[i] = Math.floor(Math.random() * 10).toString();   
+        } else if (!isNaN(did[i])) {
+            test[i] = Math.floor(Math.random() * 10).toString();
         }
     }
     did = test;
@@ -106,6 +107,8 @@ async function fetchm3files(currentGame) {
         return fetchm3files();
     }
 
+    if (autoquality) return playVideo(playerData.playURL);
+
     let m31 = await fetch(playerData.playURL);
     let text = await m31.text();
 
@@ -126,21 +129,30 @@ async function startVideo(game) {
 
 async function playVideo(src) {
     let v = document.createElement('div');
-    v.innerHTML = `<video controls autoplay></video>`;
+    v.innerHTML = `<video controls autoplay muted></video>`;
     document.body.appendChild(v);
 
     var video = v.children[0];
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) return video.src = src;
-    var hls = new Hls();
+    var hls = new Hls({
+        manifestLoadingTimeOut: 20000,
+        manifestLoadingMaxRetry: Infinity, // keep retrying forever
+        manifestLoadingRetryDelay: 2000,
+        manifestLoadingMaxRetryTimeout: 64000
+    });
     hls.attachMedia(video);
     hls.on(Hls.Events.MEDIA_ATTACHED, function () {
         console.log("video and hls.js are now bound together !");
         hls.loadSource(src);
         hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
             console.log("manifest loaded, found " + data.levels.length + " quality level");
-            video.play();
+            // video.play();
         });
     });
+
+    setTimeout(() => {
+        hls.loadSource(src);
+    }, 3000);
 }
 
 
